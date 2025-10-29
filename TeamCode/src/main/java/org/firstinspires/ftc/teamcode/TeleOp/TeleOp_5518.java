bb32e018c7e2bd8375026caacdbbb926bde6ec01
OmBhoplay123 <bhoplayom@gmail.com>
2025-10-28 22:06:09 -0400

    first commit moved code over from 25978, commented out all the arm code and deleted the auto code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp_5518.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import org.firstinspires.ftc.teamcode.Libs.AR.Arm;
+import org.firstinspires.ftc.teamcode.Libs.AR.MecanumDrive;
+
+@TeleOp(name = "5518 TeleOp", group = "TeleOp")
+public class TeleOp_5518 extends LinearOpMode
+{
+    //@Override
+    public void runOpMode()
+    {
+        MecanumDrive mecanumDrive;
+        //Arm arm;
+
+        // Initialize the drivetrain
+        mecanumDrive = new MecanumDrive(this);
+        //arm = new Arm (this);
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        while (opModeIsActive())
+        {
+            //**************************************************************************************
+            // Drive based controls are in the MecanumDrive.java file. We should strive to have controls related to a specific
+            // part of the robot contained in the library that contains the other functions of that specific part.
+
+            // ---------------------Gamepad 1 Controls ---------------------------------------------
+            // Drive control specified in Mecanum drive functions.
+
+            // ---------------------Gamepad 2 Controls ---------------------------------------------
+            // Wrist controls
+            if (gamepad2.y) {
+                //arm.setWristGuard();
+            }
+            if (gamepad2.b) {
+                //arm.setWristDrop();
+            }
+            if (gamepad2.a) {
+                //arm.setWristGrab();
+            }
+            if (gamepad2.x) {
+                //arm.setWristLow();
+            }
+
+            // Claw controls
+            if (gamepad2.left_trigger != 0 ) {
+                //arm.closeClaw();
+            }
+            if (gamepad2.right_trigger != 0) {
+                //arm.openClaw();
+            }
+
+            // Linear slide height controls
+            if (gamepad2.dpad_up){
+                //arm.moveHighBasket();
+            } else if (gamepad2.dpad_left) {
+                //arm.moveLowBasket();
+            } else if (gamepad2.dpad_down) {
+                //arm.moveGrab();
+            }
+
+            //**************************************************************************************
+            //--------------------- Per Loop Update Code -------------------------------------------
+            // Functions in this section get run every loop of the code. Anything that needs to update
+            // every loop should be in here.
+            mecanumDrive.drive(); // Update movement
+          
+
+            //**************************************************************************************
+            //--------------------- TELEMETRY Code -------------------------------------------------
+            // Useful telemetry data in case needed for testing and to find heading of robot
+            mecanumDrive.getTelemetryData();
+            //arm.getTelemetryData();
+
+            telemetry.update();
+        }
+    }
+}
